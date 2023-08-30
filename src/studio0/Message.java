d1b90d47bc9f35aa73158951c451a2a21c0b3bec
annettelu <annettelu@localhost>
2023-08-30 18:16:33 -0500

    One more attempt

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,6 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("2nd message");
 		System.out.println("Hi everyone!");
+		System.out.print("Please work!!");
 	}
 }
